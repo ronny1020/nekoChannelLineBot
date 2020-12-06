c44d335b6763648912cc89c74c26fb5ae656a841
Ronny Hsieh <1020ronny@gmail.com>
2020-12-07 01:03:44 +0800

    fix(google): fix return

--- src/feature/google.ts
+++ src/feature/google.ts
@@ -1,6 +1,8 @@
 export default async function google(message: string): Promise<string> {
   if (message.substr(0, 6).toLowerCase() === 'google') {
-    'https://www.google.com/search?q=' + message.substr(7).replace(' ', '+')
+    return (
+      'https://www.google.com/search?q=' + message.substr(7).replace(' ', '+')
+    )
   }
 
   return ''
