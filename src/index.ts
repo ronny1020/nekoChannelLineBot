a7002a8efad208ab2274c3716cf3d0e27dcccfde
Ronny Hsieh <1020ronny@gmail.com>
2020-12-06 17:04:20 +0800

    feat(main): use line sample

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,48 @@
-import express, { Application, Request, Response, NextFunction } from 'express'
+import {
+  Config,
+  Client,
+  ClientConfig,
+  middleware,
+  MiddlewareConfig,
+  WebhookEvent,
+  TextMessage,
+} from '@line/bot-sdk'
 
-// Boot express
-const app: Application = express()
-const port = 5000
+import express = require('express')
 
-// Application routing
-app.use('/', (req: Request, res: Response, ) => {
-  res.status(200).send({ data: 'Hello from Ornio AS' })
+const config: Config = {
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN!,
+  channelSecret: process.env.LINE_CHANNEL_SECRET!,
+}
+// create LINE SDK client
+const client = new Client(<ClientConfig>config)
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express()
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', middleware(<MiddlewareConfig>config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err)
+      res.status(500).end()
+    })
 })
-
-// Start server
-app.listen(port, () => console.log(`Server is listening on port ${port}!`))
+// event handler
+function handleEvent(event: WebhookEvent) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null)
+  }
+  console.log(`Received message: ${event.message.text}`)
+  // create a echoing text message
+  const echo: TextMessage = { type: 'text', text: event.message.text }
+  // use reply API
+  return client.replyMessage(event.replyToken, echo)
+}
+// listen on port
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`listening on ${port}`)
+})
