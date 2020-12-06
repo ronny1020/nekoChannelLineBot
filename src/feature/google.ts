9f42951917b1865ba4564108e8c28546584c50c6
Ronny Hsieh <1020ronny@gmail.com>
2020-12-07 00:19:38 +0800

    feat(google): multiple keywords

--- src/feature/google.ts
+++ src/feature/google.ts
@@ -1,6 +1,6 @@
 export default async function google(message: string): Promise<string> {
   if (message.substr(0, 6).toLowerCase() === 'google') {
-    return 'https://www.google.com/search?q=' + message.substr(7)
+    'https://www.google.com/search?q=' + message.substr(7).replace(' ', '+')
   }
 
   return ''
