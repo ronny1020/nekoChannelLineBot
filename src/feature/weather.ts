94bf533f80d7e53e7b066fdeda6f7f69428f9894
Ronny Hsieh <1020ronny@gmail.com>
2020-12-07 21:58:56 +0800

    fix(weather): text style

--- src/feature/weather.ts
+++ src/feature/weather.ts
@@ -9,13 +9,13 @@ export default async function weather(message: string): Promise<string> {
       `http://api.openweathermap.org/data/2.5/weather?q=Taipei&appid=${process.env.OPEN_WEATHER_API_KEY}&lang=zh_tw&units=metric`
     )
 
-    return `台北天氣${data.weather[0].description}
-    現在溫度 ${data.main.temp} 度
-    體感溫度 ${data.main.feels_like} 度
-    (最高 ${data.main.temp_min} 度 ~ 最低 ${data.main.temp_max} 度)
-    大氣壓力 ${data.main.pressure} mb
-    濕度     ${data.main.humidity} %
-    能見度   ${data.main.visibility} 公尺`
+    return `台北現在天氣${data.weather[0].description}
+現在溫度 ${data.main.temp} 度
+體感溫度 ${data.main.feels_like} 度
+(最高 ${data.main.temp_min} 度 ~ 最低 ${data.main.temp_max} 度)
+大氣壓力 ${data.main.pressure} mb
+相對濕度 ${data.main.humidity} %
+能見度     ${data.visibility} 公尺`
   }
 
   return ''
