9cc20dde4afadb940d9082e931f57ebaf3caee19
Ronny Hsieh <1020ronny@gmail.com>
2020-12-07 21:48:59 +0800

    feat(weather): weather init

---
+++ src/feature/weather.ts
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+export default async function weather(message: string): Promise<string> {
+  if (
+    ['weather', '天氣', '台北天氣'].includes(message) &&
+    process.env.OPEN_WEATHER_API_KEY
+  ) {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=Taipei&appid=${process.env.OPEN_WEATHER_API_KEY}&lang=zh_tw&units=metric`
+    )
+
+    return `台北天氣${data.weather[0].description}
+    現在溫度 ${data.main.temp} 度
+    體感溫度 ${data.main.feels_like} 度
+    (最高 ${data.main.temp_min} 度 ~ 最低 ${data.main.temp_max} 度)
+    大氣壓力 ${data.main.pressure} mb
+    濕度     ${data.main.humidity} %
+    能見度   ${data.main.visibility} 公尺`
+  }
+
+  return ''
+}
--- src/handleReply.ts
+++ src/handleReply.ts
@@ -1,6 +1,7 @@
 import google from './feature/google'
+import weather from './feature/weather'
 
 export default async function handleReply(message: string): Promise<string> {
   message = message.trim()
-  return google(message) || ''
+  return (await google(message)) || (await weather(message)) || ''
 }
