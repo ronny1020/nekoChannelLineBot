c4b8ec82a7f473a88d3557733bf9fc8bd136bfce
Ronny Hsieh <1020ronny@gmail.com>
2021-04-18 14:23:54 +0800

    refactor(flex message): add createCommonTextMessage

--- src/feature/weather.ts
+++ src/feature/weather.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { TextMessage, FlexMessage } from '@line/bot-sdk'
-import { createFlexMessage } from '../tool/createMessage'
+import createCommonTextMessage from '../tool/createCommonTextMessage'
 
 export default async function weather(
   message: string
@@ -30,169 +30,20 @@ export default async function weather(
     `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPEN_WEATHER_API_KEY}&lang=zh_tw&units=metric`
   )
 
-  return createFlexMessage(
+  return createCommonTextMessage(
     {
-      type: 'bubble',
-      size: 'kilo',
-      body: {
-        type: 'box',
-        layout: 'vertical',
-        contents: [
-          {
-            type: 'box',
-            layout: 'horizontal',
-            contents: [
-              {
-                type: 'text',
-                text: `${cityName}天氣`,
-                size: 'xl',
-                color: '#555555',
-                flex: 0,
-                weight: 'bold',
-              },
-              {
-                type: 'text',
-                text: data.weather[0].description,
-                size: 'xl',
-                color: '#111111',
-                align: 'end',
-              },
-            ],
-          },
-          {
-            type: 'separator',
-            margin: 'xs',
-          },
-          {
-            type: 'box',
-            layout: 'vertical',
-            margin: 'md',
-            spacing: 'sm',
-            contents: [
-              {
-                type: 'box',
-                layout: 'horizontal',
-                contents: [
-                  {
-                    type: 'text',
-                    text: '現在溫度',
-                    size: 'sm',
-                    color: '#555555',
-                    flex: 0,
-                  },
-                  {
-                    type: 'text',
-                    text: `${data.main.temp} 度`,
-                    size: 'sm',
-                    color: '#111111',
-                    align: 'end',
-                  },
-                ],
-              },
-              {
-                type: 'box',
-                layout: 'horizontal',
-                contents: [
-                  {
-                    type: 'text',
-                    text: '體感溫度',
-                    size: 'sm',
-                    color: '#555555',
-                    flex: 0,
-                  },
-                  {
-                    type: 'text',
-                    text: `${data.main.feels_like} 度`,
-                    size: 'sm',
-                    color: '#111111',
-                    align: 'end',
-                  },
-                ],
-              },
-              {
-                type: 'box',
-                layout: 'horizontal',
-                contents: [
-                  {
-                    type: 'text',
-                    size: 'sm',
-                    color: '#555555',
-                    flex: 0,
-                    text: ' ',
-                  },
-                  {
-                    type: 'text',
-                    text: `(最高 ${data.main.temp_max} 度 ~ 最低 ${data.main.temp_min} 度)`,
-                    size: 'sm',
-                    color: '#111111',
-                    align: 'end',
-                  },
-                ],
-              },
-              {
-                type: 'box',
-                layout: 'horizontal',
-                contents: [
-                  {
-                    type: 'text',
-                    size: 'sm',
-                    color: '#555555',
-                    flex: 0,
-                    text: '大氣壓力',
-                  },
-                  {
-                    type: 'text',
-                    text: `${data.main.pressure} mb`,
-                    size: 'sm',
-                    color: '#111111',
-                    align: 'end',
-                  },
-                ],
-              },
-              {
-                type: 'box',
-                layout: 'horizontal',
-                contents: [
-                  {
-                    type: 'text',
-                    size: 'sm',
-                    color: '#555555',
-                    flex: 0,
-                    text: '相對濕度',
-                  },
-                  {
-                    type: 'text',
-                    text: `${data.main.humidity} %`,
-                    size: 'sm',
-                    color: '#111111',
-                    align: 'end',
-                  },
-                ],
-              },
-              {
-                type: 'box',
-                layout: 'horizontal',
-                contents: [
-                  {
-                    type: 'text',
-                    size: 'sm',
-                    color: '#555555',
-                    flex: 0,
-                    text: '能見度',
-                  },
-                  {
-                    type: 'text',
-                    text: `${data.visibility} 公尺`,
-                    size: 'sm',
-                    color: '#111111',
-                    align: 'end',
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
+      title: `${cityName}天氣`,
+      subTitle: data.weather[0].description,
+      contents: [
+        { key: '現在溫度', value: `${data.main.temp} 度` },
+        { key: '體感溫度', value: `${data.main.feels_like} 度` },
+        {
+          key: '',
+          value: `(最高 ${data.main.temp_max} 度 ~ 最低 ${data.main.temp_min} 度)`,
+        },
+        { key: '相對濕度', value: `${data.main.humidity} %` },
+        { key: '能見度', value: `${data.visibility} 公尺` },
+      ],
     },
     `${cityName}天氣`
   )
---
+++ src/tool/createCommonTextMessage.ts
@@ -0,0 +1,79 @@
+import { FlexComponent, FlexMessage } from '@line/bot-sdk'
+import { createFlexMessage } from './createMessage'
+
+export interface CommonTextMessageDataContent {
+  key: string
+  keyColor?: string
+  value: string
+  valueColor?: string
+}
+
+export interface CommonTextMessageData {
+  title: string
+  subTitle?: string
+  contents: (CommonTextMessageDataContent | 'separator')[]
+}
+
+export default function createCommonTextMessage(
+  commonTextMessageData: CommonTextMessageData,
+  altText: string
+): FlexMessage {
+  const titleComponent: FlexComponent[] = [
+    {
+      type: 'text',
+      text: commonTextMessageData.title,
+      size: 'xl',
+      color: '#555555',
+      flex: 0,
+      weight: 'bold',
+    },
+  ]
+
+  if (commonTextMessageData.subTitle)
+    titleComponent.push({
+      type: 'text',
+      text: commonTextMessageData.subTitle,
+      size: 'xl',
+      color: '#111111',
+      align: 'end',
+    })
+
+  return createFlexMessage(
+    {
+      type: 'bubble',
+      size: 'kilo',
+      body: {
+        type: 'box',
+        layout: 'vertical',
+        contents: commonTextMessageData.contents.map((content) =>
+          content === 'separator'
+            ? {
+                type: 'separator',
+                margin: 'xs',
+              }
+            : {
+                type: 'box',
+                layout: 'horizontal',
+                contents: [
+                  {
+                    type: 'text',
+                    text: content.key,
+                    size: 'sm',
+                    color: content.keyColor ?? '#555555',
+                    flex: 0,
+                  },
+                  {
+                    type: 'text',
+                    text: content.value,
+                    size: 'sm',
+                    color: content.valueColor ?? '#111111',
+                    align: 'end',
+                  },
+                ],
+              }
+        ),
+      },
+    },
+    altText
+  )
+}
