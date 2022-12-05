edf0c7463a8fb2b985f5e5b92a8d745daf1fdd31
Ronny Hsieh <46809311+ronny1020@users.noreply.github.com>
2022-12-05 20:47:39 +0800

    refactor(puppeteer): use async await instead of new Promise

--- src/utility/services/puppeteer/puppeteerTool.ts
+++ src/utility/services/puppeteer/puppeteerTool.ts
@@ -1,18 +1,21 @@
 import puppeteer, { Browser, Page } from 'puppeteer'
 
+async function openPuppeteerBrowser() {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  })
+
+  console.log('Puppeteer browser is open.')
+
+  return browser
+}
+
 let puppeteerBrowserPromise: Promise<Browser>
 
 export function getPuppeteerBrowser() {
   if (puppeteerBrowserPromise) return puppeteerBrowserPromise
 
-  puppeteerBrowserPromise = new Promise((resolve) => {
-    resolve(
-      puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      })
-    )
-    console.log('Puppeteer browser is open')
-  })
+  puppeteerBrowserPromise = openPuppeteerBrowser()
 
   return puppeteerBrowserPromise
 }
