44f11b45873c247d8c2ff5c2646ee4ac48ced3b4
Gagu <gagu@insowe.com>
2024-01-24 18:52:41 +0800

    feature(version): use env

--- src/modules/version.ts
+++ src/modules/version.ts
@@ -1,6 +1,5 @@
 import { TextMessage } from '@line/bot-sdk'
 import { createTextMessage } from '@utility/services/line/createMessage'
-import fs from 'fs'
 
 export default async function version(
   message: string
@@ -8,13 +7,5 @@ export default async function version(
   if (message.toLowerCase() !== '!version') {
     return undefined
   }
-
-  let appVersion = ''
-  try {
-    appVersion =
-      fs.readFileSync('version.txt', 'utf8') || 'version.txt is empty'
-  } catch (err) {
-    appVersion = 'version.txt not found'
-  }
-  return createTextMessage(appVersion)
+  return createTextMessage(process.env.RENDER_GIT_COMMIT || 'no version found')
 }
